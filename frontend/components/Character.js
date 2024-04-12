9d9f96dbcca4e30b0ef640cf69a3486e0f177465
3GGYlol <xlj135@gmail.com>
2024-04-12 15:29:31 -0400

    wip

--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import { useState } from 'react'
 
+function Character(props) { 
+ const {character} = props;
+ const [planetToggle, setPlanetToggle] = useState(false);
+
+const toggle= () => {
+  setPlanetToggle(!planetToggle);
+  }
 function Character() { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
